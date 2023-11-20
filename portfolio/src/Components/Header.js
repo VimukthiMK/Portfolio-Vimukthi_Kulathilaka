4ea2c1f04e8d448ddbbe3d398c5b6f0a6ff7486e
VimukthiMK <vimukthikulathiulaka@gmail.com>
2023-11-21 01:38:30 +0530

    Header.js

--- portfolio/src/Components/Header.js
+++ portfolio/src/Components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+    <><nav id="desktop-nav">
+      <div className="logo">Vimukthi</div>
+      <div>
+        <ul className="nav-links">
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/experience">Experience</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+      </>
   )
 }
