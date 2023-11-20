9585cc147cced9cc238ec506b0b0fa5d744ddcc9
VimukthiMK <vimukthikulathiulaka@gmail.com>
2023-11-21 01:15:43 +0530

    Created all routes for navigation

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
+import Header from './Components/Header';
+import Profile from './Components/Profile';
+import About from './Components/About';
+import Experience from './Components/Experience';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" exact component={Profile} />
+          <Route path="/about" component={About} />
+          <Route path="/experience" component={Experience} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/contact" component={Contact} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
