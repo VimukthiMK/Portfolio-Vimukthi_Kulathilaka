49de195dfdc061878a74e9777c24cce7d83fe8f2
VimukthiMK <vimukthikulathiulaka@gmail.com>
2023-11-21 01:45:11 +0530

    Navigation CSS

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from 'react'
+import'./CSS/style.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Header from './Components/Header';
