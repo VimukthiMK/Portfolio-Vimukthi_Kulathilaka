e784ae1d5ea22124fff3087ff0a5ff64d13c3fb2
VimukthiMK <vimukthikulathilaka@gmail.com>
2023-11-22 23:04:24 +0530

    Modified app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import './CSS/style.css';
-import { BrowserRouter as Router, Routes,  Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 import Header from './Components/Header';
 import Profile from './Components/Profile';
@@ -11,12 +10,13 @@ import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 
 function App() {
+
   return (
     <Router>
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" exact element={<Profile />} />
+          <Route path="/" element={<Profile />} />
           <Route path="/about" element={<About />} />
           <Route path="/experience" element={<Experience />} />
           <Route path="/projects" element={<Projects />} />
